c059b7db1593e3a56f13aa41c6dc94e2189d67f6
David Slusher <fdslusher@gmail.com>
2021-10-08 17:35:22 -0600

    stuck

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function convert(){
     var element1 = document.getElementById("input1");
     var element2 = document.getElementById("c1");
+    var element3 = document.getElementsByName("far-cel");
+    var element4 = document.getElementById("farenheit");
+
+    var check3 = element3.value;
+    var check4 = element4.onclick;
+
+    alert(check3 + "    " + check4);
 
     var num1 = element1.value;
     var num3 = ((num1 - 32) * 5) / 9;
